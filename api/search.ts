dc8b0a00e246a024639f282a1ab2df63d0fae6c1
mateomckee <mateomckee64@gmail.com>
2024-04-02 16:42:47 -0500

    Reject unauthorized connections

--- api/search.ts
+++ api/search.ts
@@ -4,7 +4,7 @@ import { Pool } from 'pg';
 const pool = new Pool({
   connectionString: process.env.POSTGRES_URL,
   ssl: {
-    rejectUnauthorized: false //only for development, remove for production
+    rejectUnauthorized: true
   }
 });
 
